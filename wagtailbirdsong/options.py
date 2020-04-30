3d94886d071387b0a24d2a7c997943fa860cc4db
Seb <seb@neonjungle.studio>
2020-04-30 17:43:10 +1000

    More changes to more reflect the modeladmin approach

--- wagtailbirdsong/options.py
+++ wagtailbirdsong/options.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 
 from wagtail.contrib.modeladmin.helpers import AdminURLHelper, ButtonHelper
 from wagtail.contrib.modeladmin.options import ModelAdmin
-
+from wagtailbirdsong.backends.smtp import SMTPEmailBackend
 from .views import editor, mail
-
+from .models import Contact
 
 class EmailCampaignButtonHelper(ButtonHelper):
     def get_buttons_for_obj(self, obj, **kwargs):
@@ -70,36 +70,46 @@ class EmailCampaignButtonHelper(ButtonHelper):
         return buttons
 
 
-
 class EmailAdmin(ModelAdmin):
     list_display = ('subject',)
     button_helper_class = EmailCampaignButtonHelper
     inspect_view_enabled = True
     inspect_view_class = editor.InspectCampaign
     inspect_template_name = 'wagtailbirdsong/editor/inspect_campaign.html'
+    backend_class = SMTPEmailBackend
+    contact_class = Contact
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.backend = self.backend_class()
 
     def get_admin_urls_for_registration(self):
         urls = super().get_admin_urls_for_registration()
         urls = (
-            url(self.url_helper.get_action_url_pattern('view_draft'), self.view_draft, name=self.url_helper.get_action_url_name('view_draft')), 
-            url(self.url_helper.get_action_url_pattern('confirm_send'), self.confirm_send, name=self.url_helper.get_action_url_name('confirm_send')),
-            url(self.url_helper.get_action_url_pattern('send_campaign'), self.send_campaign, name=self.url_helper.get_action_url_name('send_campaign')),
-            url(self.url_helper.get_action_url_pattern('confirm_test'), self.confirm_test, name=self.url_helper.get_action_url_name('confirm_test')),
-            url(self.url_helper.get_action_url_pattern('send_test'), self.send_test, name=self.url_helper.get_action_url_name('send_test')),
-            
+            url(self.url_helper.get_action_url_pattern('view_draft'), self.view_draft,
+                name=self.url_helper.get_action_url_name('view_draft')),
+            url(self.url_helper.get_action_url_pattern('confirm_send'), self.confirm_send,
+                name=self.url_helper.get_action_url_name('confirm_send')),
+            url(self.url_helper.get_action_url_pattern('send_campaign'), self.send_campaign,
+                name=self.url_helper.get_action_url_name('send_campaign')),
+            url(self.url_helper.get_action_url_pattern('confirm_test'), self.confirm_test,
+                name=self.url_helper.get_action_url_name('confirm_test')),
+            url(self.url_helper.get_action_url_pattern('send_test'), self.send_test,
+                name=self.url_helper.get_action_url_name('send_test')),
+
         ) + urls
 
         return urls
 
     def view_draft(self, request, instance_pk):
         campaign = self.model.objects.get(pk=instance_pk)
-        contact = campaign.get_contact_model().objects.first()
+        contact = self.contact_class.objects.first()
         return editor.view_draft(request, campaign, contact)
 
     def confirm_send(self, request, instance_pk):
         campaign = self.model.objects.get(pk=instance_pk)
         return editor.confirm_send(
-            request, 
+            request,
             campaign,
             self.url_helper.get_action_url('send_campaign', instance_pk=instance_pk),
             self.url_helper.get_action_url('index')
@@ -107,22 +117,21 @@ class EmailAdmin(ModelAdmin):
 
     def send_campaign(self, request, instance_pk):
         campaign = self.model.objects.get(pk=instance_pk)
-        return mail.send_campaign(request, campaign)
+        # TODO Filtering will be done here
+        contacts = self.contact_class.objects.all()
+        return mail.send_campaign(self.backend, request, campaign, contacts)
 
     def confirm_test(self, request, instance_pk):
         campaign = self.model.objects.get(pk=instance_pk)
         return editor.confirm_test(
-            request, 
+            request,
             campaign,
-            self.url_helper.get_action_url('send_test', instance_pk=instance_pk),
+            self.url_helper.get_action_url('send_test', instance_pk),
             self.url_helper.get_action_url('index')
         )
 
     def send_test(self, request, instance_pk):
         campaign = self.model.objects.get(pk=instance_pk)
-        if not request.POST.get('test_email', False):
+        if request.method == 'GET':
             return self.confirm_test(request, instance_pk)
-        return mail.send_test(request, campaign)
-
-    
-    
+        return mail.send_test(self.backend, request, campaign)
--- wagtailbirdsong/views/mail.py
+++ wagtailbirdsong/views/mail.py
@@ -7,12 +7,6 @@ from wagtail.contrib.modeladmin.helpers.url import AdminURLHelper
 from ..models import Receipt, Contact
 
 
-def send_helper(request, campaign, subject, contacts):
-    mail_backend = campaign.get_backend()
-
-    return mail_backend.send_email(request, campaign, subject, contacts)
-
-
 def redirect_helper(campaign):
     url_helper = AdminURLHelper(type(campaign))
     campaign_list_url = url_helper.get_action_url('index')
@@ -20,30 +14,36 @@ def redirect_helper(campaign):
     return redirect(campaign_list_url)
 
 
-def send_campaign(request, campaign):
-    contacts = campaign.get_contact_model().objects.all()
-    success = send_helper(request, campaign, campaign.subject, contacts)
+def send_campaign(backend, request, campaign, contacts):
+    success = backend.send_campaign(
+        request, campaign, campaign.subject, contacts)
 
+    Receipt.objects.bulk_create([
+        Receipt(campaign=campaign, contact=c, success=success) for c in contacts
+    ])
     if success:
         for contact in contacts:
             Receipt.objects.create(campaign=campaign, contact=contact, sent_date=timezone.now())
-        messages.add_message(request, messages.INFO, f"Campaign with ID {campaign.id} sent")
+        messages.add_message(
+            request, messages.SUCCESS, f"Campaign '{campaign.name}' sent to {len(contacts)} contacts")
     else:
-        messages.add_message(request, messages.ERROR, f"Campaign with ID {campaign.id} failed to send")
+        messages.add_message(request, messages.ERROR,
+                             f"Campaign '{campaign.name}' failed to send")
 
     return redirect_helper(campaign)
 
 
-def send_test(request, campaign):
+def send_test(backend, request, campaign):
     test_email = request.POST.get('test_email', False)
-    test_contact = Contact.objects.create(email=test_email) # no .save() so don't create a dead contact in the db
-    success = send_helper(request, campaign, f"[TEST] {campaign.subject}.", [test_contact])
+
+    # FIXME this won't work with a custom contact model
+    test_contact = Contact(email=test_email)
+    success = backend.send_campaign(
+        request, campaign, f"[TEST] {campaign.subject}.", [test_contact])
 
     if success:
-        messages.add_message(request, messages.INFO, f"Test email sent, please check your inbox")
+        messages.add_message(request, messages.SUCCESS, f"Test email sent, please check your inbox")
     else:
         messages.add_message(request, messages.ERROR, f"Test email failed to send")
-    
-    return redirect_helper(campaign)
 
-    
+    return redirect_helper(campaign)
