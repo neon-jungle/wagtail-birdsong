a59ed8a7b02b85ceb6fc6deee94795154cd19875
Seb <seb@neonjungle.studio>
2020-08-18 10:46:25 +1000

    Don't mark test emails as completed

--- birdsong/views/actions.py
+++ birdsong/views/actions.py
@@ -16,13 +16,14 @@ def send_campaign(backend, request, campaign, contacts):
     campaign.status = CampaignStatus.SENDING
     campaign.save()
     backend.send_campaign(request, campaign, contacts)
+    messages.success(request, f"Campaign sent to {len(contacts)} contacts")
 
     return redirect_helper(campaign)
 
 
 def send_test(backend, request, campaign, test_contact):
     campaign.subject = f"[TEST] {campaign.subject}"
-    backend.send_campaign(request, campaign, [test_contact])
+    backend.send_campaign(request, campaign, [test_contact], test_send=True)
     messages.success(request, "Test email sent, please check your inbox")
 
     return redirect_helper(campaign)
