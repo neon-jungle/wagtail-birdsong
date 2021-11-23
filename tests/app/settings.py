56a6e8013e540ebec62ba50e5f1b8406844431cf
Seb <seb@neonjungle.studio>
2021-11-23 14:12:48 +1100

    little deps update

--- tests/app/settings.py
+++ tests/app/settings.py
@@ -60,8 +60,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'wagtail.core.middleware.SiteMiddleware',
 ]
 
 TEMPLATES = [
