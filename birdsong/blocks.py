5e28aaf5db06f4998255a1550ab60d6d4f5a4b86
veronika <v@pop-os.localdomain>
2022-12-01 11:19:24 +0100

    change formatting

--- birdsong/blocks.py
+++ birdsong/blocks.py
@@ -7,30 +7,30 @@ class UnwrappedStreamBlock(blocks.StreamBlock):
     """Removes the surrounding divs around streamblocks."""
 
     def render_basic(self, value, context=None):
-        return mark_safe("\n".join(child.render(context=context) for child in value))
+        return mark_safe('\n'.join(child.render(context=context) for child in value))
 
 
 class DefaultBlocks(UnwrappedStreamBlock):
     rich_text = blocks.RichTextBlock(
-        template="birdsong/mail/blocks/richtext.html",
-        features=["h3", "h4", "bold", "italic", "link", "ul", "ol", "document-link"],
+        template='birdsong/mail/blocks/richtext.html',
+        features=['h3', 'h4', 'bold', 'italic', 'link', 'ul', 'ol', 'document-link'],
     )
 
 
 class NewsletterSubscriptionBlock(blocks.StructBlock):
     """streamfield block for newsletter subscription on wagtail pages."""
 
-    headline = blocks.CharBlock(verbose_name=_("Headline"), max_length=80)
+    headline = blocks.CharBlock(verbose_name=_('Headline'), max_length=80)
     subtext = blocks.CharBlock(
-        verbose_name=_("Subtext"), max_length=150, required=False
+        verbose_name=_('Subtext'), max_length=150, required=False
     )
-    button_label = blocks.CharBlock(label=_("Button label"), max_length=50)
-    privacy_text = blocks.CharBlock(verbose_name=_("Privacy hint"), max_length=60)
+    button_label = blocks.CharBlock(label=_('Button label'), max_length=50)
+    privacy_text = blocks.CharBlock(verbose_name=_('Privacy hint'), max_length=60)
     privacy_page = blocks.PageChooserBlock(
-        verbose_name=_("Privacy page"), max_length=100
+        verbose_name=_('Privacy page'), max_length=100
     )
 
     class Meta:
-        template = "birdsong/site/blocks/signup.html"
-        icon = "mail"
-        label = _("Newsletter subscription")
+        template = 'birdsong/site/blocks/signup.html'
+        icon = 'mail'
+        label = _('Newsletter subscription')
