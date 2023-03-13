ff3df5cca94b65b66dc19f3c0edaeafc2cf18ff5
David Smejkal <david@smejkal.software>
2023-03-13 19:39:14 +0000

    Redefined BaseEmailBackend as a true abstract class that will now require certain methods to be implemented. Better to do this early when there's just one core backend in this app.

--- birdsong/backends/__init__.py
+++ birdsong/backends/__init__.py
@@ -1,9 +1,28 @@
+from abc import ABCMeta, abstractmethod
+
 from django.conf import settings
 
 
-class BaseEmailBackend:
-    def send_email(self, request, campaign, subject, contacts):
-        raise NotImplementedError
+class BaseEmailBackend(metaclass=ABCMeta):
+    """
+    Abstract class that should be inheritted by all BIRDSONG_BACKENDs.
+    """
+
+    @abstractmethod
+    def send_campaign(self, request, campaign, contacts, test_send=False):
+        """Required be implemented in all backends.
+        
+        NOTE: Used by Send Campaign action (see `birdsong.options.CampaignAdmin.send_campaign`).
+        """
+        raise NotImplementedError("You must implement send_campaign() in your BIRDSONG_BACKEND")
+
+    @abstractmethod
+    def send_mail(self, subject, template, contact, context):
+        """Required to be implemented in all backends.
+        
+        NOTE: Utilized by Subscribe Form (see `birdsong.views.subscribe`, `birdsong.views.subscribe_api`).
+        """
+        raise NotImplementedError("You must implement send_mail() in your BIRDSONG_BACKEND")
 
     @property
     def from_email(self):
