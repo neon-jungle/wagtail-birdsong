034cb84bdeede7de777c05fc485bda9ce0987636
David Smejkal <david@smejkal.software>
2023-03-15 13:52:54 +0000

    Tried to avoid merge conflicts with  branch (will still cause conflict in )

--- birdsong/conf.py
+++ birdsong/conf.py
@@ -1,13 +1,6 @@
 from django.conf import settings
-import django.utils.module_loading as module_loading
-from django.utils.translation import gettext as _
 
 ### BIRDSONG_ADMIN ###
 BIRDSONG_ADMIN_GROUP = getattr(settings, 'BIRDSONG_ADMIN_GROUP', True)
 
-### BIRDSONG_CONTACT ###
-BIRDSONG_CONTACT_MODEL = getattr(settings, 'BIRDSONG_CONTACT_MODEL', 'birdsong.Contact')
-
-### BIRDSONG_TEST_CONTACT ###
 BIRDSONG_TEST_CONTACT = getattr(settings, 'BIRDSONG_TEST_CONTACT', { 'email': 'wagtail.birdsong@example.com' })
-
